221138898b1999b54e1855d9d6702eecfeaa85dc
Sujan <najussujan@gmail.com>
2015-09-22 16:36:51 -0500

    UI changes

--- WebContent/register.php
+++ WebContent/register.php
@@ -1,16 +1,22 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start ();
 include ("db-connection.php");
 /* @var $_POST type */
 $username = filter_input ( INPUT_POST, "name" );
 $password = filter_input ( INPUT_POST, "password" );
+$confirmpassword = filter_input ( INPUT_POST, "confirmpassword" );
 $email = filter_input ( INPUT_POST, "email" );
 $currentDate = date ( 'Y-m-d' );
+
+if (empty ( $username ) || empty ( $password ) || empty ( $confirmpassword ) || empty ( $email )) {
+	$_SESSION ['error'] = "Fields cannot be empty";
+	header ( "Location: signup.php" );
+	exit ();
+} else if ($password !== $confirmpassword) {
+	$_SESSION ['error'] = "Passwords do not match";
+	header ( "Location: signup.php" );
+	exit ();
+}
 try {
 	$selectStmnt = $db->prepare ( "SELECT username FROM users WHERE username= :username" );
 	$selectStmnt->execute ( array (
@@ -29,6 +35,8 @@ try {
 				':createddate' => $currentDate 
 		) );
 	}
+	$_SESSION ['error'] = "Signup complete";
+	header ( "Location: index.php" );
 } catch ( Exception $e ) {
 	echo 'Caught exception: ', $e->getMessage (), "\n";
 }
---
+++ WebContent/signup.php
@@ -0,0 +1,77 @@
+<?php
+session_start ();
+require ("top.html");
+
+$error = null;
+if (isset ( $_SESSION ['error'] )) {
+	$error = $_SESSION ['error'];
+	unset ( $_SESSION ['error'] );
+}
+?>
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<head>
+<title>Sign up</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div class="container text-center pagination-centered">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Todo list app</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<form action="register.php" method="POST">
+					<fieldset>
+						<legend> New User Sign up: </legend>
+						<?php if ($error): ?>
+						<div>
+							<?= $error?>
+						</div>
+						<?php endif; ?>
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								<p>
+									<strong>Username: </strong><input type="text" name="name"
+										maxlength="16" />
+								</p>
+								<p>
+									<strong>E-mail: </strong><input type="email" name="email"
+										maxlength="16" />
+								</p>
+								<p>
+									<strong>Password: </strong><input type="password"
+										name="password" maxlength="16" />
+								</p>
+								<p>
+									<strong>Confirm: </strong><input type="password"
+										name="confirmpassword" maxlength="16" />
+								</p>
+								<p>
+									<input type="submit" class="btn btn-success" value="Sign Up" />
+								</p>
+								<p>
+									<a href="index.php">Login</a>
+								</p>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+<?php
+require ("bottom.html");
+?>
