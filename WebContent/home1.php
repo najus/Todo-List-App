8f1cc1283020bbd5db0568e56339538d51a72b64
Sachin Kushwaha <sachinkeshav@gmail.com>
2015-09-22 22:28:58 -0500

    Logout added.

--- WebContent/home1.php
+++ WebContent/home1.php
@@ -35,6 +35,7 @@ and open the template in the editor.
     	<div class="container text-center pagination-centered">
 			<div class="col-lg-12">
 				<h1>Todo List App</h1>
+				<h2><span class="col-lg-offset-9"><a href="logout.php">Logout</a></span></h2>
 			</div>
 		</div>
         <div class="container">
